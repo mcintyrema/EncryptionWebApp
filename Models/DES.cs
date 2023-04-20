0dd231d55852ae80e7d029c428042989c90c6e08
mam838 <mam838@pitt.edu>
2023-04-20 10:34:39 -0400

    updates

---
+++ Models/DES.cs
@@ -0,0 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+namespace EncryptionWebApp.Models;
+
+public class DES{
+}
---
+++ Pages/DES.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EncryptionWebApp.Pages
+{
+    public class DESModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
